73627f490d66d2fe8a2934378e04efd96b1250bd
filipmlynarski <fmynarski@gmail.com>
2018-08-19 23:14:01 +0200

    Delete test_cards.py

--- test_cards.py
+++
@@ -1,9 +0,0 @@
-import warnings
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
-
-import pandas as pd
-
-#valiadte csv file
-df = pd.read_csv('cards.csv')
-print(df.ix[0:4])
